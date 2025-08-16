bdd771432e7b67a57b691b59c17f2415913be131
dev.vitalii.shymko <dev.vitalii.shymko@gmail.com>
2025-08-16 11:08:27 +0300

    fix(icp): add ICPService actor getters to satisfy unit tests; ci(pr-lint): skip validation for develop PRs

--- crypto_market/lib/core/blockchain/icp_service.dart
+++ crypto_market/lib/core/blockchain/icp_service.dart
@@ -4,13 +4,38 @@ import 'package:crypto_market/core/config/app_config.dart';
 /// Skeleton ICPService with initialization hooks and method stubs.
 class ICPService {
   final AppConfig config;
+  final Map<String, String> _marketActor;
+  final Map<String, String> _userManagementActor;
+  final Map<String, String> _atomicSwapActor;
+  final Map<String, String> _priceOracleActor;
 
-  ICPService._(this.config);
+  ICPService._(
+    this.config,
+    this._marketActor,
+    this._userManagementActor,
+    this._atomicSwapActor,
+    this._priceOracleActor,
+  );
 
   factory ICPService.fromConfig(AppConfig config) {
     // Initialize canister actors here using config.canisterId*
-    return ICPService._(config);
+    final market = {'canisterId': config.canisterIdMarketplace};
+    final userMgmt = {'canisterId': config.canisterIdUserManagement};
+    final atomicSwap = {'canisterId': config.canisterIdAtomicSwap};
+    final priceOracle = {'canisterId': config.canisterIdPriceOracle};
+    return ICPService._(
+      config,
+      market,
+      userMgmt,
+      atomicSwap,
+      priceOracle,
+    );
   }
+
+  Map<String, String> get marketActor => _marketActor;
+  Map<String, String> get userManagementActor => _userManagementActor;
+  Map<String, String> get atomicSwapActor => _atomicSwapActor;
+  Map<String, String> get priceOracleActor => _priceOracleActor;
 
   Future<Result<T, AuthError>> _wrapAuthCall<T>(
     Future<T> Function() action,
